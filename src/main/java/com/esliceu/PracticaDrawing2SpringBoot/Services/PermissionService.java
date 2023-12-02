9546b922080dd40e92218b11fb64be956af58edd
JoanLlompart <jllompartsocias@cifpfbmoll.eu>
2023-12-02 11:55:04 +0100

    Aplicat alguns estils a els botons i la llista de view

--- src/main/java/com/esliceu/PracticaDrawing2SpringBoot/Services/PermissionService.java
+++ src/main/java/com/esliceu/PracticaDrawing2SpringBoot/Services/PermissionService.java
@@ -6,9 +6,25 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PermissionService {
-
    private String user_email;
    private String permissionType;
+   private int idCanvas;
+
+   public PermissionService(){}
+
+    public PermissionService(String user_email, String permissionType, int idCanvas) {
+        this.user_email = user_email;
+        this.permissionType = permissionType;
+        this.idCanvas = idCanvas;
+    }
+
+    public int getIdCanvas() {
+        return idCanvas;
+    }
+
+    public void setIdCanvas(int idCanvas) {
+        this.idCanvas = idCanvas;
+    }
 
     public String getPermissionType() {
         return permissionType;
---
+++ src/main/resources/static/js/fetch.js
@@ -0,0 +1,27 @@
+ function sendReadRequest(id, name) {
+        fetch(`/viewCanvas/read?id=${id}&user_email=${name}`, {
+            method: 'POST',
+            credentials: 'same-origin'
+            // Puedes añadir más opciones como headers, body, etc., según sea necesario
+        }).then(response => {
+            // Manejar la respuesta del servidor si es necesario
+            console.log('Solicitud de lectura enviada');
+        }).catch(error => {
+            // Manejar errores si la solicitud falla
+            console.error('Error al enviar solicitud de lectura:', error);
+        });
+    }
+
+    function sendWriteRequest(id, name) {
+        fetch(`/viewCanvas/write?id=${id}&nameCanvas=${name}`, {
+            method: 'POST',
+            credentials: 'same-origin'
+            // Puedes añadir más opciones como headers, body, etc., según sea necesario
+        }).then(response => {
+            // Manejar la respuesta del servidor si es necesario
+            console.log('Solicitud de escritura enviada');
+        }).catch(error => {
+            // Manejar errores si la solicitud falla
+            console.error('Error al enviar solicitud de escritura:', error);
+        });
+    }
