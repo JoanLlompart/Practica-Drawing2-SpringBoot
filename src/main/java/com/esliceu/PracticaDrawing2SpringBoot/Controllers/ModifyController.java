9523237038ebb22cd40ad676f46bf47849d23b9e
JoanLlompart <jllompartsocias@cifpfbmoll.eu>
2023-12-03 11:00:14 +0100

    Arreglant bug de que es pot tornar a donar permisos si ja en te

--- src/main/java/com/esliceu/PracticaDrawing2SpringBoot/Controllers/ModifyController.java
+++ src/main/java/com/esliceu/PracticaDrawing2SpringBoot/Controllers/ModifyController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ModifyController {
     @Autowired
     CanvasServices canvasServices;
-
     @Autowired
     CanvasVersionDTO canvasVersionDTO;
     @Autowired
