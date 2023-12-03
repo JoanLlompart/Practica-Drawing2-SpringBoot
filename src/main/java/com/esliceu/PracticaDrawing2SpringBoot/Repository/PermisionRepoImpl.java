4768fbca35f39c226adb8d4d1de166420a659c62
JoanLlompart <jllompartsocias@cifpfbmoll.eu>
2023-12-03 11:45:08 +0100

    Arreglant bug de que es pot tornar a donar permisos si ja en te

--- src/main/java/com/esliceu/PracticaDrawing2SpringBoot/Repository/PermisionRepoImpl.java
+++ src/main/java/com/esliceu/PracticaDrawing2SpringBoot/Repository/PermisionRepoImpl.java
@@ -18,19 +18,33 @@ public class PermisionRepoImpl implements PermissionRepo{
         String user_email = permission.getUser_email();
         System.out.println(user_email);
         String permissionType = permission.getPermissionType();
-
-
             // Verificar si existe un registro en la tabla Canvas con idObjectes y user_email correspondientes
             String canvasCheckQuery = "SELECT COUNT(*) FROM Canvas WHERE idObjectes = ? AND user_email = ?";
-            int count = jdbcTemplate.queryForObject(canvasCheckQuery, Integer.class, idCanvas, owner_email);
-            if (count > 0) {
+            int countPropCanvas = jdbcTemplate.queryForObject(canvasCheckQuery, Integer.class, idCanvas, owner_email);
+            if (countPropCanvas<0) return false;
+
+
+            String sql = "INSERT INTO Permission (idCanvas, user_email, permissionType) " +
+                    "VALUES (?, ?, ?) " +
+                    "ON DUPLICATE KEY UPDATE permissionType = ?";
+            int rowsAffected = jdbcTemplate.update(sql, idCanvas, user_email, permissionType,permissionType);
+            return rowsAffected > 0; // Devuelve true si se insertó satisfactoriamente
+
+          /*  if (count > 0) {
                 // Si existe un registro en la tabla Canvas que coincide con los parámetros proporcionados,
                 // realizar la inserción en la tabla Permission
+                System.out.println("Actualitzat");
+                String updatePermissionQuery = "UPDATE Permission SET permissionType = ? WHERE idCanvas = ? AND user_email = ?";
+                int rowsAffected = jdbcTemplate.update(updatePermissionQuery, permissionType, idCanvas, user_email);
+                return rowsAffected > 0;
+            } else {
+                System.out.println("Nou insert");
+                // Si no existe un registro en la tabla Permission, realizar la inserción
                 String insertPermissionQuery = "INSERT INTO Permission (idCanvas, user_email, permissionType) VALUES (?, ?, ?)";
                 int rowsAffected = jdbcTemplate.update(insertPermissionQuery, idCanvas, user_email, permissionType);
-
                 return rowsAffected > 0; // Devuelve true si se insertó satisfactoriamente
             }
-            return false;
+
+           */
     }
 }
